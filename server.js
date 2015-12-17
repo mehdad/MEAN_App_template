313c75729e73d368811eb0b157bb72869a51237b
mehdad <m.mohamadi313@gmail.com>
2015-12-18 03:23:04 +0330

    sample Middlewares Added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express=require('./config/express')
+process.env.NODE_ENV = process.env.NODE_ENV || "development" ; 
 
-var app=express()
+var express=require('./config/express');
+
+var app=express();
 app.listen(6767);
 
 console.log('server running at http://localhost:6767');
