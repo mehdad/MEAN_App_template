39388ba43f9eed2730f77a99dd97bcbe0413891f
mehdad <m.mohamadi313@gmail.com>
2015-12-18 00:11:49 +0330

    MVC Structure implemented

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var express67=require('express')
-var app=express67()
-app.use('/',function(req,res){
-	res.send("Hello MEAN");
-}).listen(6767);
+var express=require('./config/express')
+
+var app=express()
+app.listen(6767);
 
 console.log('server running at http://localhost:6767');
 
