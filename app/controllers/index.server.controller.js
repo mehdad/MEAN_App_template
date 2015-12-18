bd04de407653c01c09fd837ca2f2ef564e7f9d73
mehdad <m.mohamadi313@gmail.com>
2015-12-18 20:26:07 +0330

    session Added

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,9 @@
 exports.render =function(req,res){
+	if(req.session.lastvisitssss){
+		console.log(req.session.lastvisitssss);
+	}
+	req.session.lastvisitssss ='session is:' + new Date();
+
 	res.render('index', { 
 		title: 'Hello World.This is Just A test'
 	});
--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
-var express = require('express'),
+var config = require('./config'),
+	express = require('express'),
 	morgan = require('morgan'),
 	compress = require('compression'),
 	bodyParser = require('body-parser'),
-	methodOverride = require('method-override');
+	methodOverride = require('method-override'),
+	session = require('express-session');
 
 
 module.exports = function(){
@@ -18,6 +20,12 @@ module.exports = function(){
 	}));
 	app.use(bodyParser.json());
 	app.use(methodOverride());
+
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret
+	}));
 
 	app.set('views','./app/views');
 	app.set('view engine','ejs');
