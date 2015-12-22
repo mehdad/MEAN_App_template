172a1c8579f7231504551137a8ba27b02448949d
mehdad <m.mohamadi313@gmail.com>
2015-12-22 13:49:05 +0330

    first angular tempalte added

--- public/application.js
+++ public/application.js
@@ -1,5 +1,5 @@
 var mainApplicationModuleName = 'tempate_app';
-var mainApplicationModue = angular.module(mainApplicationModuleName , []);
+var mainApplicationModue = angular.module(mainApplicationModuleName , ['example']);
 
 angular.element(document).ready(function(){
 	angular.bootstrap(document, [mainApplicationModuleName]);
