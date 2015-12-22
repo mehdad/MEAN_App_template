0a7a17511bd104ae3af64a1c2ab1bb2be13168dd
mehdad <m.mohamadi313@gmail.com>
2015-12-22 16:42:05 +0330

    angular-route module added and implemented

--- public/application.js
+++ public/application.js
@@ -1,5 +1,11 @@
 var mainApplicationModuleName = 'tempate_app';
-var mainApplicationModue = angular.module(mainApplicationModuleName , ['example']);
+var mainApplicationModue = angular.module(mainApplicationModuleName , ['ngRoute','example']);
+
+mainApplicationModue.config(['$locationProvider',
+	function($locationProvider){
+		$locationProvider.hashPrefix('!');
+	}
+]);
 
 angular.element(document).ready(function(){
 	angular.bootstrap(document, [mainApplicationModuleName]);
